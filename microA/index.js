568b690fb70eeea189fd21d9a97236b776c6360d
michal-kaszubowski <michal.mateusz.kaszubowski@gmail.com>
2023-06-12 17:48:01 +0200

    Requesting assistance!

--- microA/index.js
+++ microA/index.js
@@ -9,7 +9,14 @@ app.get('/', (req, res) => {
 		.catch(err => console.error(err));
 });
 
-app.get('/checkme', (req, res) => res.send('Healthy'));
+app.get('/checkme', (req, res) => res.send('Healthy\n'));
+
+app.get('/checkhim', (req, res) => {
+	const request = fetch('http://localhost:3001/check');
+	request
+		.then(response => res.send('Healthy\n'))
+		.catch(err => console.error(err));
+});
 
 const port = 3000;
 app.listen(port, () => console.log(`Server is UP & RUNNING on port ${port}!`));
--- microB/index.js
+++ microB/index.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
-// require('dotenv').config();
 
 const root = process.env.MONGO_USERNAME;
 const rootPwd = process.env.MONGO_PASSWORD;
 const authMethod = 'DEFAULT';
-const uri = `mongodb://${root}:${rootPwd}@localhost:27017/?authMechanism=${authMethod}`;
+const dbHost = process.env.MONGO_HOST;
+const dbPort = process.env.MONGO_PORT;
+
+const uri = `mongodb://${root}:${rootPwd}@${dbHost}:${dbPort}/?authMechanism=${authMethod}`;
 
 const client = new MongoClient(uri);
-const db = client.db(`${root}`);
-const collection = db.collection('sampleCollection');
+// const db = client.db(`${root}`);
+// const collection = db.collection('sampleCollection');
 
 const app = express();
 
 app.get('/', (req, res) => {
-	collection.find({}).toArray()
-		.then(response => res.send(response))
-		.catch(err => console.error(err));
+	client.connect().then(connected => res.send('Connected to MongoDB :)')).catch(err => res.send(err));
 });
 
 app.get('/check', (req, res) => res.send('Healthy'));
